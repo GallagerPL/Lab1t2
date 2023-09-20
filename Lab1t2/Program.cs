8bc9f93c39b1fd5d1d41db28e9d5c8add5b3ccbd
GallagerPL <116964637+GallagerPL@users.noreply.github.com>
2023-09-20 20:56:39 +0300

    Добавьте файлы проекта.

---
+++ Lab1t2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Заданы массивы D и E. Вычислить и вывести значения fi=(2di+sinei)/di, где i=1,2,…,16; вывести 1<fi<3.*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Random random = new Random();   
+
+        double[] D = new double[16];
+        double[] F = new double[16];
+
+        for(int i = 0;i<16;i++)
+        {
+            D[i] = random.Next(0,20);
+            F[i] = random.Next(0,20);
+        }
+
+        for (int i = 0; i < 16; i++)
+        {
+           Console.WriteLine(D[i]);
+        }
+    }
+}
